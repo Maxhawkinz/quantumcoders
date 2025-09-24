437ca8c48550e0556ac39e9dc54c07e36fedd0fa
Ajay <ajaythakare500@gmail.com>
2025-09-24 20:23:48 +0530

    Update AuthModal.tsx

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -1,5 +1,4 @@
 'use client'
-
 import { useState } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import { X, Mail, Lock, User, Eye, EyeOff, CheckCircle } from 'lucide-react'
